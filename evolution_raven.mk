36cefc5380b01f0668620748861d50d6902b9d07
AnierinB <anierin@evolution-x.org>
2023-01-25 23:58:22 +0000

    raviole: Initialize Evolution X 8.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- evolution_raven.mk
+++ evolution_raven.mk
@@ -4,24 +4,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Evolution X stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_6 := true
+TARGET_PIXEL_STAND_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_raven.mk)
-$(call inherit-product, device/google/gs101/lineage_common.mk)
+$(call inherit-product, device/google/gs101/evolution_common.mk)
 
-include device/google/raviole/raven/device-lineage.mk
+include device/google/raviole/raven/device-evolution.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6 Pro
-PRODUCT_NAME := lineage_raven
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
+PRODUCT_NAME := evolution_raven
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
--- oriole/BoardConfigEvolution.mk
+++ oriole/BoardConfigEvolution.mk
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-include device/google/raviole/BoardConfigLineage.mk
+include device/google/raviole/BoardConfigEvolution.mk
 
 include vendor/google/oriole/BoardConfigVendor.mk
